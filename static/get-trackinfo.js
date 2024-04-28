b4bd04b4a88fab40d06f8f67d7cc10bb3e13a613
貴雪❆ <71214511+javascriptjp@users.noreply.github.com>
2024-04-28 16:33:39 +0900

    fix: The url of the playlist would be included.

--- static/get-trackinfo.js
+++ static/get-trackinfo.js
@@ -4,6 +4,14 @@ new Promise(resolve => {
     const authorEl = document.querySelector('.playbackSoundBadge__lightLink');
     const artworkEl = document.querySelector(".playbackSoundBadge__avatar .image .sc-artwork");
     if (!playEl || !playEl.classList.contains('playing') || !titleEl || !authorEl || !artworkEl) resolve("undefined");
-    const content = { title: titleEl.title, author: authorEl.title, artwork: artworkEl.style.backgroundImage.replace('url("', '').replace('")', ''), url: titleEl.href };
+    const title = titleEl.title;
+    const author = authorEl.title;
+    const artwork = artworkEl.style.backgroundImage.replace('url("', '').replace('")', '');
+    const url = ((url) => {
+        const parsedUrl = new URL(url);
+        parsedUrl.search = '';
+        return parsedUrl.toString();
+    })(titleEl.href);
+    const content = { title: title, author: author, artwork: artwork, url: url };
     resolve(JSON.stringify(content));
 });
